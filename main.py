ad16836b32c16958822bfe87d6203a3340c404f9
jaxburd <jburd@ucsb.edu>
2022-03-06 15:31:52 -0800

    Jackie's changes (Wavelets)

---
+++ main.py
@@ -0,0 +1,33 @@
+# installl dependencies necessary
+import streamlit as st
+import numpy as np
+
+# Title
+st.title("Pyramids and Wavelets")
+
+# TODO: Enter our names here
+
+# TODO: Image Resampling - Page 1
+    # Interpolation
+
+    # Decimation
+
+
+
+# TODO: Pyramids - Page 2
+    # Gaussian
+
+    # Laplacian
+
+
+# TODO: Wavelets - Page 3
+    # TODO: Continuous
+
+    # TODO: Discontinuous
+
+# Conclusion
+        # Potential Application
+            # Slider Tool Blending of faces
+            # Embossing - Laplacian Pyramid
+            # Blending of face - gaussian and lapacan pyramid
+                # Make up imaginary people
